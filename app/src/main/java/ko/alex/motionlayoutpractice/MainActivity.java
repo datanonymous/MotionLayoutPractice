96a420fda715e8a7f8c68a7b289113a2f2530441
Alex Ko <ko.e.alexander@gmail.com>
2019-04-28 15:18:59 -0400

    1

--- app/src/main/java/ko/alex/motionlayoutpractice/MainActivity.java
+++ app/src/main/java/ko/alex/motionlayoutpractice/MainActivity.java
@@ -12,5 +12,11 @@ public class MainActivity extends AppCompatActivity {
         //https://github.com/googlesamples/android-ConstraintLayoutExamples
         //https://medium.com/google-developers/introduction-to-motionlayout-part-i-29208674b10d
         //https://medium.com/google-developers/defining-motion-paths-in-motionlayout-6095b874d37
+
+        //SCENE 1 BASIC MOVEMENT
+        //SCENE 2 SAME AS SCENE 1, BUT EVERYTHING IS IN ONE SCENE XML FILE
+        //SCENE 3 CHANGING COLOR, SAME BASIC MOVEMENT
+        //SCENE 4 WITH HUMP //todo: https://medium.com/google-developers/defining-motion-paths-in-motionlayout-6095b874d37
+        //SCENE 5 WITH THE LOOP DE LOOP
     }
 }
